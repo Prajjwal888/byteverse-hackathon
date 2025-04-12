f95dd697c7ea40a5080e34e15a36e8179e0719b3
Manas-Bhutada <manasbhutada19@gmail.com>
2025-04-12 16:52:39 +0530

    add donor dsahboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,11 @@
+import React from 'react';
+import DonorPage from './pages/DonorPage/DonorPage.jsx';
+import AllShow from './pages/DonorPage/AllShow.jsx';
 function App() {
   return (
     <>
+    {/* DonorPage */}
+      <AllShow/>
     </>
   )
 }
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LandingPage from './pages/LandingPage/LandingPage.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* <LandingPage/> */}
     <App />
+    
   </StrictMode>,
 )
